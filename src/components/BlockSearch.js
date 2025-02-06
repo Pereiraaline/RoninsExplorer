fa13a576f8aea9edbb2336a84031de513932e303
Vinicius <146675540+vinimaffra03@users.noreply.github.com>
2025-02-05 21:03:20 -0300

    Update BlockSearch.js

--- src/components/BlockSearch.js
+++ src/components/BlockSearch.js
@@ -1,25 +1,27 @@
 import React, { useState } from "react";
 
 const BlockSearch = () => {
-  const [blockNumber, setBlockNumber] = useState("");
   const [blockData, setBlockData] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   const handleSearch = async () => {
     setError(null);
     setBlockData(null);
-  
+    setLoading(true);
+
     try {
       const response = await fetch(
-        `https://1095-34-95-219-131.ngrok-free.app/block/ledger/${blockNumber}`
+        "https://1095-34-95-219-131.ngrok-free.app/block/ledger"
       );
-  
+
       console.log("Response status:", response.status);
       console.log("Response headers:", response.headers);
-  
-      const text = await response.text(); // Lê a resposta como texto primeiro
+
+      const text = await response.text();
       console.log("Response body:", text);
-  
-      // Tenta converter para JSON somente se o conteúdo for JSON válido
+
+      // Tenta converter para JSON
       try {
         const data = JSON.parse(text);
         setBlockData(data);
@@ -28,44 +30,23 @@ const BlockSearch = () => {
       }
     } catch (err) {
       setError(err.message);
+    } finally {
+      setLoading(false);
     }
   };
-//   const handleSearch = async () => {
-//     setError(null);
-//     setBlockData(null);
-
-//     try {
-//       const response = await fetch(
-//         `https://1095-34-95-219-131.ngrok-free.app/block/ledger/${blockNumber}`
-//       );
-
-//       if (!response.ok) {
-//         throw new Error("Bloco não encontrado ou erro na API");
-//       }
-
-//       const data = await response.json();
-//       setBlockData(data);
-//     } catch (err) {
-//       setError(err.message);
-//     }
-// };
 
   return (
     <div className="search">
-      <h3>Search Block by Number</h3>
-      <input
-        type="number"
-        value={blockNumber}
-        onChange={(e) => setBlockNumber(e.target.value)}
-        placeholder="Enter Block Number"
-      />
-      <button onClick={handleSearch}>Search</button>
+      <h3>Consultar Último Bloco</h3>
+      <button onClick={handleSearch} disabled={loading}>
+        {loading ? "Carregando..." : "Buscar Bloco"}
+      </button>
 
       {error && <p style={{ color: "red" }}>{error}</p>}
 
       {blockData && (
         <div className="result">
-          <h4>Block Information</h4>
+          <h4>Informações do Bloco</h4>
           <p>
             <strong>Block Number:</strong> {blockData.sequence}
           </p>
